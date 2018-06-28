0a1fe674904709b4aa7676996f6a0190e7086551
Kelly Schroeder <kellyrschroeder@gmail.com>
2018-06-28 01:44:14 -0600

    Adds main.bundle.js

--- main.bundle.js
+++ main.bundle.js
@@ -414,7 +414,7 @@
 	  if (host === "localhost" || host === "127.0.0.1") {
 	    return "http://localhost:3000";
 	  } else {
-	    return "https://quantified-self-rails-kellys-dev.us-west-2.elasticbeanstalk.com";
+	    return "https://quantified-self-retry.us-west-2.elasticbeanstalk.com";
 	  }
 	};
 
